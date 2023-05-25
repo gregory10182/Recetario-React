f894042495df09ae8bb4b0cf1b621eba31adf8a3
gregory10182 <Gregory10183@gmail.com>
2023-05-25 19:46:23 -0400

    new api baseurl

--- src/components/api/apiRecetas.js
+++ src/components/api/apiRecetas.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-const baseUrl = "https://18.224.25.38/receta/";
+const baseUrl = "https://gregorypf.site/receta/";
 
 const getAll = () => {
   const request = axios.get(baseUrl);
