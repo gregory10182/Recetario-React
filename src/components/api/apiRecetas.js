6c42c608daf850cf15abb802024d559a9ff908e4
Gregory Pérez <Gregory10183@gmail.com>
2024-01-29 19:37:45 -0300

    changing api url

--- src/components/api/apiRecetas.js
+++ src/components/api/apiRecetas.js
@@ -1,6 +1,6 @@
 import axios from "axios";
-const baseUrl = "http://localhost:8000/receta/";
-// const baseUrl = "https://gregorypf.site/receta/";
+// const baseUrl = "http://localhost:8000/receta/";
+const baseUrl = "https://gregorypf.site/receta/";
 
 const getAll = () => {
   const request = axios.get(baseUrl);
